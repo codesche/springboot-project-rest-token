dfc2cba983b5da097b87ee44c014138d865931eb
codesche <codesche@gmail.com>
2024-09-10 15:56:50 +0900

    폴더 경로 변경

--- rest_blog/src/main/java/com/kosta/service/FavoriteService.java
+++ rest_blog/src/main/java/com/kosta/service/FavoriteService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import com.kosta.domain.FavoriteRequest;
-import com.kosta.domain.FavoriteResponse;
+import com.kosta.domain.request.FavoriteRequest;
+import com.kosta.domain.response.FavoriteResponse;
 
 public interface FavoriteService {
 
