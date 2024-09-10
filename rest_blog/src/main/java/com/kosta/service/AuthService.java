ca0a9e28125db20bc9eac6ce698fea2fe9ea0561
codesche <codesche@gmail.com>
2024-09-10 15:56:13 +0900

    refreshToken 로직 추가

--- rest_blog/src/main/java/com/kosta/service/AuthService.java
+++ rest_blog/src/main/java/com/kosta/service/AuthService.java
@@ -1,8 +1,13 @@
 package com.kosta.service;
 
 import java.util.List;
+import java.util.Map;
 
-import com.kosta.domain.*;
+import com.kosta.domain.request.SignUpRequest;
+import com.kosta.domain.request.UserDeleteRequest;
+import com.kosta.domain.request.UserUpdateRequest;
+import com.kosta.domain.response.UserResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthService {
 
@@ -15,6 +20,9 @@ public interface AuthService {
 	void deleteUser(UserDeleteRequest userDeleteRequest);
 
 	boolean duplicateCheckEmail(String email);
+
+	Map<String, String> refreshToken(HttpServletRequest req);
+
 
 //    LoginResponse login(String email, String password);
 }
