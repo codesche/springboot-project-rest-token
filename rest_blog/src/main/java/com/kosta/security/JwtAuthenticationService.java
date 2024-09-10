d3c1d289b63ca3408becdcb9aff6bee705749375
codesche <codesche@gmail.com>
2024-09-10 15:57:41 +0900

    폴더 경로 변 + 리프레시 토큰 로직 적용

--- rest_blog/src/main/java/com/kosta/security/JwtAuthenticationService.java
+++ rest_blog/src/main/java/com/kosta/security/JwtAuthenticationService.java
@@ -1,8 +1,8 @@
-package com.kosta.config;
+package com.kosta.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kosta.domain.LoginResponse;
+import com.kosta.domain.response.LoginResponse;
 import com.kosta.entity.User;
+import com.kosta.repository.UserRepository;
 import com.kosta.util.TokenUtils;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +17,23 @@ import java.util.Map;
 public class JwtAuthenticationService {
 
     private final TokenUtils tokenUtils;
+    private final UserRepository userRepository;
 
     void successAuthentication(HttpServletResponse response, Authentication authResult) throws IOException {
         User user = (User) authResult.getPrincipal();
         Map<String, String> tokenMap = tokenUtils.generateToken(user);
-        String accessToken = tokenMap.get("accessToken");                   // 토큰 가져오기
+        String accessToken = tokenMap.get("accessToken");                   // 액세스 토큰 가져오기
+        String refreshToken = tokenMap.get("refreshToken");                 // 리프레시 토큰 가져오기
+
+        // 리프레시 토큰을 DB에 저장!
+        user.setRefreshToken(refreshToken);
+        userRepository.save(user);
+
+        tokenUtils.setRefreshTokenCookie(response, refreshToken);
 
         LoginResponse loginResponse = LoginResponse.builder()
-                .accessToken(accessToken)
-                .build();
+                    .accessToken(accessToken)
+                    .build();
         tokenUtils.writeResponse(response, loginResponse);
     }
 
